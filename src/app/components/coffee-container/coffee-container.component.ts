92eb0551ec1222c51274d37e8bea0ba4e84e0506
jcarrizoagustin <jcarrizoagustin@gmail.com>
2024-08-09 14:28:57 -0300

    Agrego logica para hacer el switch entre botones

--- src/app/components/coffee-container/coffee-container.component.ts
+++ src/app/components/coffee-container/coffee-container.component.ts
@@ -2,24 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { GetProductsService } from '../../services/get-products.service';
 import { Coffee } from '../../model/coffee.interface';
 import { CoffeCardComponent } from '../coffe-card/coffe-card.component';
+import { tap } from 'rxjs';
+import { ActivatedButtonEnum } from '../../model/enums/activatedButton.enum';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coffee-container',
   standalone: true,
-  imports: [CoffeCardComponent],
+  imports: [CoffeCardComponent,CommonModule],
   templateUrl: './coffee-container.component.html',
   styleUrl: './coffee-container.component.css'
 })
 export class CoffeeContainerComponent implements OnInit{
 
-  products !:Coffee[];
+  allProducts !:Coffee[];
+  availableProducts !: Coffee[];
+  activatedButton !: ActivatedButtonEnum;
 
   constructor(private getProducts: GetProductsService){}
 
   ngOnInit():void {
     this.getProducts.getProducts().subscribe(
-      (data:Coffee[]) => this.products = data
+      (data:Coffee[]) => this.allProducts = data
     )
+    this.activatedButton = ActivatedButtonEnum.ALL;
+  }
+
+
+  handleClickButtons(newActivatedButton: string):void {
+    if(newActivatedButton == 'ALL'){
+      this.activatedButton = ActivatedButtonEnum.ALL
+    }else{
+      this.activatedButton = ActivatedButtonEnum.AVAILABLE
+    }
+  }
+
+  buttonSelectedIsAll(): boolean {
+    return this.activatedButton == ActivatedButtonEnum.ALL
+  }
+
+  buttonSelectedIsAvailable(): boolean {
+    return this.activatedButton == ActivatedButtonEnum.AVAILABLE
   }
 
 }
